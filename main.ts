fcae08e2322f9d46d759583b045d08e4a0ace44c
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-02-02 19:45:21 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 
+//% color=190 weight=#b5d136 icon="\uf055" block="Concatenation"
+namespace concatenation {
+    export function concatenate(x: Array<any>, y: Array<any>): Array<any> {
+        return x.concat(y);
+    }
+    
+    export function extend(x: Array<any>, y: Array<any>): void {
+        x = concatenate(x, y);
+    }
+
+    export function concatenate_many(items: Array<Array<any>>): Array<any> {
+        let result: Array<any> = [];
+        items.forEach((value, index) => {
+            result.concat(value);
+        })
+        return result
+    }
+}

5eb80c2e61d5817717733b67e3072c7d568b57fc
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-02-02 20:01:48 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,13 @@
 //% color=#b5d136 weight=0 icon="\uf055" block="Concatenation"
 namespace concatenation {
     //% blockId=concat_concatenate
-    //% block="$x=variables_get(list) joined with $y=variables_get(list)"
+    //% block="%x=variables_get(list) joined with %y=variables_get(list1)"
     export function concatenate(x: Array<any>, y: Array<any>): Array<any> {
         return x.concat(y);
     }
     
     //% blockId=concat_extend
-    //% block="concatenate $y=variables_get(list) to the end of $x=variables_get(list)"
+    //% block="concatenate %y=variables_get(list) to the end of %x=variables_get(list1)"
     export function extend(x: Array<any>, y: Array<any>): void {
         x = concatenate(x, y);
     }

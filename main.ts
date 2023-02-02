a49f5f0d4f98848191b20596e6b06b5ffbdc8db0
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2023-02-02 20:15:32 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,23 @@
-
+/*
+* Extension for easily concatenating arrays of any type
+* in blocks.
+*/
 //% color=#5282d1 weight=0 icon="\uf055" block="Concatenation"
 namespace concatenation {
+    /*
+    * Join two arrays together and return the result
+    */
     //% blockId=concat_concatenate
     //% block="%x=variables_get(list) joined with %y=variables_get(list1)"
     export function concatenate(x: Array<any>, y: Array<any>): Array<any> {
         return x.concat(y);
     }
-    
-    //% blockId=concat_extend
-    //% block="concatenate %y=variables_get(list) to the end of %x=variables_get(list1)"
-    export function extend(x: Array<any>, y: Array<any>): void {
-        x = concatenate(x, y);
-    }
 
+    /*
+    * @param {Array<any>[]} items - List of arrays to join
+    * Flatten an array of arrays into single value;
+    * Returns the result.
+    */
     //% blockId=concat_concatenate_many
     //% block="concatenate list of arrays %items=variables_get(list)"
     export function concatenate_many(items: Array<Array<any>>): Array<any> {
